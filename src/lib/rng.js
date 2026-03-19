239a05ba689c3c737a504aa79f05caed71d465de
Rob Herman <sitnaltax@gmail.com>
2026-03-18 23:07:57 -0400

    roll over at New York time, not UTC

--- src/lib/rng.js
+++ src/lib/rng.js
@@ -17,6 +17,5 @@ export function getDailySeed(dateStr) {
 }
 
 export function todayUTC() {
-  const d = new Date();
-  return `${d.getUTCFullYear()}-${String(d.getUTCMonth()+1).padStart(2,'0')}-${String(d.getUTCDate()).padStart(2,'0')}`;
+  return new Intl.DateTimeFormat('en-CA', { timeZone: 'America/New_York' }).format(new Date());
 }
